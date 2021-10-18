2751c63a1c629590455d65217b6e2250e3d20e61
nidameltem95@gmail.com <nidameltem95@gmail.com>
2021-10-19 00:05:33 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,33 @@
+console.log("Merhaba kodlama.io")
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+dolarDun = "9.20"
+
+{
+    let dolarDun = 9.10
+}
+
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 bu olmaz
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut kredisi","Emlak Konut kredisi","kamu konut kredisi","özel konut kredisi"]
+
+console.log("<ul>")
+for (let i=0;i<konutKredileri.length;i++) {
+    
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
+
+
+
+
